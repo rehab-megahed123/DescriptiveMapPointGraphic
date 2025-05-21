499ad8043daa047b75426b0794877d28f4064147
rehab-megahed123 <rehabmegahed241@gmail.com>
2025-05-21 21:55:34 +0200

    create DescriptivePoint model

---
+++ Descriptive Map Point Graphic/DescriptivePoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Descriptive_Map_Point_Graphic
+{
+    public class DescriptivePoint
+    {
+        public string Description { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
